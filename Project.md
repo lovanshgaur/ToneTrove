b26112139af8d52797c1ac0fbde4616bb9b32723
lovanshgaur <lovanshgaur@gmail.com>
2025-07-31 10:05:16 +0530

    FinalCommit-V01

---
+++ Project.md
@@ -0,0 +1,34 @@
+
+
+
+
+
+So i am kinda good in html,css and js required for frontend, like i am good for DOM manipulation but I dont have the thought process of a senior developer which stops me from achieving what i could otherwise i know coding.
+I am more focused with vanilla js and want to be a core js developer but yes i have tried and made projects on react.
+
+What kind of roles or freelancing gigs are you aiming for?
+For now i want to just start earning more money, and i am good with static websites so more focused on frontend but i am also learning backend and working on fullstack projects
+i am kinda lost and as i am a good frontend dev but my web designing is really poor, like if i can have the design i can make it but not from scratch and i am not an organisation so i cant afford a web designer for just my projects.
+i have worked on many static websites and RESTAPIS, made my own api called greekgeek which provides database for greek gods, monster and places.
+i have multiple project ideas which are kinda incomplete ranging from 15% to 80% completion but first i want to get the senior developer mindset and learn how to professionally make a project from scratch, everything needed to do in a proffesional way, we can start it by taking one of my pre-exisitng incomplete project and restarting it from scratch.
+
+
+
+TONETROVE by lavi
+The purpose is to create a tool for users to take pre-build web-themes with 4 colors in one theme, user can check demo, of any theme he likes.
+So i will tell you what i have. i have a simple page which shows 50+ themes each with unique name and 4 colors, background, text, accent and theme.
+user can come to see the themes select one, get css code or copy a single color if he likes.
+
+it also in addition to it have a second tool which takes and image as input and then generates primary colors from it
+
+Colorhunt and colors.io are similar tools from whom i took inspiration.
+
+feature is simple, devs like me dont have to try-catch many colors combination, they can take a pre-build combo from here.
+if you have a picture you wanna base our design on, you can do that too from our another tool.
+
+i have a simple wireframe ready but i'll like if you help me with design in start.
+
+I have the first minimal version in which both tools are separate.
+
+here is the link of tool-01 ==> https://lovanshgaur.github.io/Tone-Trove_v0.1/
+
---
+++ js/main.js
@@ -0,0 +1,103 @@
+let colorContainer = document.getElementById('color-container')
+let main = document.querySelector('main')
+
+fetch("assets/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(data.colorPalettes);
+        let colors = data.colorPalettes
+        if (colors) {
+            fillContainer(colors)
+        }
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+
+function isColorDark(color) {
+    const rgb = parseInt(color.slice(1), 16);
+    const r = (rgb >> 16) & 0xff;
+    const g = (rgb >> 8) & 0xff;
+    const b = (rgb >> 0) & 0xff;
+    const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    return luminance < 128;
+}
+
+function fillContainer(data) {
+    data.forEach(color => {
+        // console.log(color.colors)
+        console.log(color.paletteName.trim().replace(/\s+/g, '').toLowerCase());
+
+        const colorBox = document.createElement('div')
+        const stripesContainer = document.createElement('div')
+        const colorDetail = document.createElement('div')
+        const btns = document.createElement('div')
+        btns.classList.add('buttons');
+        const copyBtn = document.createElement('button')
+        const demoBtn = document.createElement('button')
+
+        copyBtn.innerText = 'Copy '
+        demoBtn.innerText = 'Demo'
+
+        btns.appendChild(copyBtn)
+        btns.appendChild(demoBtn)
+
+        colorBox.classList.add('color-box')
+        stripesContainer.classList.add('stripes-container')
+        colorDetail.classList.add('detail-container')
+
+        let colorName = document.createElement('h3')
+        colorName.innerText = color.paletteName
+        let colorNameLink = color.paletteName.trim().replace(/\s+/g, '').toLowerCase()
+        colorName.addEventListener('click', () => {
+            const link = `html/demo.html?data=${colorNameLink}`;
+            window.location.href = link;
+        })
+
+        let stripes = color.colors;
+        colorHover(stripes[2], stripes[1],stripes[0])
+        stripes.forEach(stripe => {
+            const stripeBox = document.createElement('div')
+            let stripesName = document.createElement('div')
+            stripeBox.classList.add('stripe')
+            stripeBox.style.background = stripe
+            stripesName.innerText = stripe
+            stripesName.style.color = isColorDark(stripe) ? "white" : "black";
+            stripeBox.appendChild(stripesName)
+            stripesContainer.appendChild(stripeBox)
+
+            stripeBox.addEventListener('click', () => {
+                navigator.clipboard.writeText(`${stripe}`);
+                let comment = document.getElementById("comment");
+                comment.style.opacity = 1;
+                comment.textContent = "Copied!";
+                setTimeout(() => {
+                    comment.style.opacity = 0;
+                }, 1500);
+            })
+            stripeBox.addEventListener('mouseover', () => {
+                colorName.style.color = isColorDark(stripe) ? "white" : "black";
+                main.style.background = stripe
+            })
+            stripeBox.addEventListener('mouseleave', () => {
+                colorName.style.color = "black";
+                main.style.background = "rgb(255, 255, 255)"
+            })
+
+        })
+        function colorHover(copyColor,demoColor,shadow) {
+            copyBtn.style.backgroundColor = copyColor
+            demoBtn.style.backgroundColor = demoColor
+            copyBtn.style.boxShadow = `1px 1px 2px ${shadow}`
+            demoBtn.style.boxShadow = `1px 1px 2px ${shadow}`
+            copyBtn.style.color = isColorDark(copyColor) ? "white" : "black";
+            demoBtn.style.color = isColorDark(demoColor) ? "white" : "black";
+        }
+
+        colorBox.setAttribute("id", colorNameLink);
+        colorDetail.appendChild(colorName)
+        colorBox.appendChild(stripesContainer)
+        colorBox.appendChild(colorDetail)
+        colorBox.appendChild(btns)
+
+        colorContainer.appendChild(colorBox)
+    })
+}
